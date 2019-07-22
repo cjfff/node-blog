ad8f1d77e132c4e48a893afd27fcde176b5b00fa
unknown <865553742@qq.com>
2019-07-22 09:10:33 +0800

    feat: express morgan 日志工具使用

--- express-server/app.js
+++ express-server/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 var cookieParser = require("cookie-parser");
 const resMiddleware = require("./middware/resMiddleware.js");
 var logger = require("morgan");
@@ -15,13 +16,23 @@ var userRouter = require("./routes/user");
 
 var app = express();
 
+if (process.env.NODE_ENV !== "dev") {
+  const fileName = path.join(__dirname, "logs", "access.log");
+  const writeStream = fs.createWriteStream(fileName, {
+    flags: "a"
+  });
+  app.use(
+    logger("combined", {
+      stream: writeStream
+    })
+  );
+} else {
+  app.use(logger("dev"));
+}
 // view engine setup
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "jade");
 
-app.use(logger("combined", {
-  stream: process.stdout
-}));
 app.use(resMiddleware());
 app.use(express.json()); // 解析 application/json 获取 post data 中的数据
 app.use(express.urlencoded({ extended: false })); // 解析 form
